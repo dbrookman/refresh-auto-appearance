17fb3c2903bb26cfe2c061c28f3c5cffe5680bfd
Daniel Brookman <dannntrax@gmail.com>
2021-03-01 11:49:50 -0500

    Revert to theme check / set
    Toggling automatic alone didn't seem to change apps stuck in the wrong
    theme.

--- refresh-auto-theme.m
+++ refresh-auto-theme.m
@@ -1,12 +1,23 @@
 #import <Foundation/Foundation.h>
 
 BOOL SLSGetAppearanceThemeSwitchesAutomatically(void);
+BOOL SLSGetAppearanceThemeLegacy(void);
+
 void SLSSetAppearanceThemeSwitchesAutomatically(BOOL);
+void SLSSetAppearanceThemeLegacy(BOOL);
 
 int main() {
 
 	if (SLSGetAppearanceThemeSwitchesAutomatically()) {
 		SLSSetAppearanceThemeSwitchesAutomatically(NO);
+
+		// if dark mode is on
+		if (SLSGetAppearanceThemeLegacy()) {
+			SLSSetAppearanceThemeLegacy(YES);
+		} else {
+			SLSSetAppearanceThemeLegacy(NO);
+		}
+
 		SLSSetAppearanceThemeSwitchesAutomatically(YES);
 	}
 
