8cc51cdd4e17b72131c140104b95b1c991d5dfac
Daniel Brookman <dannntrax@gmail.com>
2021-02-28 19:48:11 -0500

    Remove theme check in favor of only toggling auto

--- fix-auto-theme.m
+++ fix-auto-theme.m
@@ -1,26 +1,13 @@
 #import <Foundation/Foundation.h>
 
-BOOL SLSGetAppearanceThemeLegacy(void);
 BOOL SLSGetAppearanceThemeSwitchesAutomatically(void);
-
-void SLSSetAppearanceThemeLegacy(BOOL);
 void SLSSetAppearanceThemeSwitchesAutomatically(BOOL);
 
 int main() {
 
 	if (SLSGetAppearanceThemeSwitchesAutomatically()) {
-
 		SLSSetAppearanceThemeSwitchesAutomatically(NO);
-
-		// if dark mode is on
-		if (SLSGetAppearanceThemeLegacy()) {
-			SLSSetAppearanceThemeLegacy(YES);
-		} else {
-			SLSSetAppearanceThemeLegacy(NO);
-		}
-
 		SLSSetAppearanceThemeSwitchesAutomatically(YES);
-
 	}
 
 	return 0;
